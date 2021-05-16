74aea6d4d04572bc4433b3142fdb1d318dec12c2
patrykasz <80646865+patrykasz@users.noreply.github.com>
2021-05-16 14:51:33 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel3.Controls.Add(this.numericUpDown1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.radioButton10);
+            this.panel3.Controls.Add(this.radioButton9);
+            this.panel3.Controls.Add(this.radioButton8);
+            this.panel3.Controls.Add(this.radioButton7);
+            this.panel3.Controls.Add(this.radioButton6);
+            this.panel3.Controls.Add(this.radioButton5);
+            this.panel3.Controls.Add(this.radioButton4);
+            this.panel3.Controls.Add(this.radioButton3);
+            this.panel3.Controls.Add(this.radioButton2);
+            this.panel3.Controls.Add(this.radioButton1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(629, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(172, 312);
+            this.panel3.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(90, 252);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(46, 23);
+            this.numericUpDown1.TabIndex = 16;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Filtry statyczne :";
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(9, 256);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(71, 19);
+            this.radioButton10.TabIndex = 11;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "Mediana";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(90, 231);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(46, 19);
+            this.radioButton9.TabIndex = 10;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "Min";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(9, 231);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(48, 19);
+            this.radioButton8.TabIndex = 9;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Max";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(9, 191);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(65, 19);
+            this.radioButton7.TabIndex = 8;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Laplace";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(9, 141);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(81, 19);
+            this.radioButton6.TabIndex = 7;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Sobel pion";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(9, 166);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(97, 19);
+            this.radioButton5.TabIndex = 6;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Sobel poziom";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(9, 116);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(105, 19);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Prewitt poziom";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(9, 91);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(89, 19);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Prewitt pion";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(9, 66);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(108, 19);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Roberts poziom";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(9, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(92, 19);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Roberts pion";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Modyfikuj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Otwórz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackColor = System.Drawing.Color.Gray;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(312, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(311, 312);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(141, 123);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-5, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(311, 312);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(141, 123);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 311);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private int width = 0, height = 0;
+        Bitmap bitmap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bitmap = (Bitmap)Bitmap.FromFile(openFileDialog1.FileName);
+                width = bitmap.Width;
+                height = bitmap.Height;
+                pictureBox1.Image = bitmap;
+            }
+        }
+        private void modyfikuj(double[,] M)
+        {
+            double pomoc_r = 0;
+            double pomoc_g = 0;
+            double pomoc_b = 0;
+            Bitmap bitmap2 = (Bitmap)bitmap.Clone();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    pomoc_r = 0;
+                    pomoc_g = 0;
+                    pomoc_b = 0;
+                    for (int k = -1; k <= 1; k++)
+                    {
+                        for (int l = -1; l <= 1; l++)
+                        {
+                            int x1 = i + k;
+                            int y1 = j + l;
+                            if (x1 < 0)
+                            {
+                                x1 = 0;
+                            }
+                            if (x1 >= height)
+                            {
+                                x1 = height - 1;
+                            }
+                            if (y1 < 0)
+                            {
+                                y1 = 0;
+                            }
+                            if (y1 >= width)
+                            {
+                                y1 = width - 1;
+                            }
+                            Color p = bitmap.GetPixel(y1, x1);
+                            int r = p.R;
+                            int g = p.G;
+                            int b = p.B;
+                            pomoc_r += r * M[k + 1, l + 1];
+                            pomoc_g += g * M[k + 1, l + 1];
+                            pomoc_b += b * M[k + 1, l + 1];
+                        }
+                    }
+
+                    double r1 = pomoc_r;
+                    double g1 = pomoc_g;
+                    double b1 = pomoc_b;
+
+                    if (r1 > 255)
+                    {
+                        r1 = 255;
+                    }
+                    if (r1 < 0)
+                    {
+                        r1 = 0;
+                    }
+                    if (g1 > 255)
+                    {
+                        g1 = 255;
+                    }
+                    if (g1 < 0)
+                    {
+                        g1 = 0;
+                    }
+                    if (b1 > 255)
+                    {
+                        b1 = 255;
+                    }
+                    if (b1 < 0)
+                    {
+                        b1 = 0;
+                    }
+
+                    bitmap2.SetPixel(j, i, Color.FromArgb((int)r1, (int)g1, (int)b1));
+                }
+            }
+            pictureBox2.Image = bitmap2;
+        }
+
+        private void max()
+        {
+            int radius = (int)numericUpDown1.Value;
+            Bitmap bitmap2 = (Bitmap)bitmap.Clone();
+
+            for (int x = 0; x < width - 1; ++x)
+            {
+                int x0 = Math.Max(1, x - radius);
+                int x1 = Math.Min(width - 1, x + radius);
+                for (int y = 0; y < height - 1; ++y)
+                {
+                    int y0 = Math.Max(0, y - radius);
+                    int y1 = Math.Min(height - 1, y + radius);
+                    Color max_pixel = bitmap.GetPixel(x, y);
+                    for (int u = x0; u < x1; ++u)
+                    {
+                        for (int v = y0; v < y1; ++v)
+                        {
+                            if (bitmap.GetPixel(u, v).R > max_pixel.R & bitmap.GetPixel(u, v).G > max_pixel.G & bitmap.GetPixel(u, v).B > max_pixel.B)
+                            {
+                                max_pixel = bitmap.GetPixel(u, v);
+                            }
+                        }
+                    }
+                    bitmap2.SetPixel(x, y, max_pixel);
+                }
+            }
+            pictureBox2.Image = bitmap2;
+        }
+
+        private void min()
+        {
+            int radius = (int)numericUpDown1.Value;
+            Bitmap bitmap2 = (Bitmap)bitmap.Clone();
+
+            for (int x = 0; x < width - 1; ++x)
+            {
+                int x0 = Math.Max(1, x - radius);
+                int x1 = Math.Min(width - 1, x + radius);
+                for (int y = 0; y < height - 1; ++y)
+                {
+                    int y0 = Math.Max(0, y - radius);
+                    int y1 = Math.Min(height - 1, y + radius);
+                    Color max_pixel = bitmap.GetPixel(x, y);
+                    for (int u = x0; u < x1; ++u)
+                    {
+                        for (int v = y0; v < y1; ++v)
+                        {
+                            if (bitmap.GetPixel(u, v).R < max_pixel.R & bitmap.GetPixel(u, v).G < max_pixel.G & bitmap.GetPixel(u, v).B < max_pixel.B)
+                            {
+                                max_pixel = bitmap.GetPixel(u, v);
+                            }
+                        }
+                    }
+                    bitmap2.SetPixel(x, y, max_pixel);
+                }
+            }
+            pictureBox2.Image = bitmap2;
+        }
+
+        private void median()
+        {
+
+            int radius = (int)numericUpDown1.Value;
+            Color[] matrix = new Color[((2 * radius) + 1) * ((2 * radius) + 1)];
+            Bitmap bitmap2 = (Bitmap)bitmap.Clone();
+            int index = 0;
+            for (int x = 0; x < width - 1; ++x)
+            {
+                int x0 = Math.Max(1, x - radius);
+                int x1 = Math.Min(width - 1, x + radius);
+                for (int y = 0; y < height - 1; ++y)
+                {
+                    int y0 = Math.Max(0, y - radius);
+                    int y1 = Math.Min(height - 1, y + radius);
+                    Color max_pixel = bitmap.GetPixel(x, y);
+                    for (int u = x0; u <= x1; ++u)
+                    {
+                        for (int v = y0; v <= y1; ++v)
+                        {
+                            matrix[index] = bitmap.GetPixel(u, v);
+                            index++;
+                        }
+                    }
+                    index = 0;
+                    int ind = Median(matrix);
+                    bitmap2.SetPixel(x, y, matrix[ind]);
+                    Array.Clear(matrix, 0, matrix.Length);
+                }
+            }
+            pictureBox2.Image = bitmap2;
+        }
+
+        int Median(Color[] xs)
+        {
+            Color[] x2 = new Color[xs.Length];
+            SortedList mySL = new SortedList();
+            for (int i = 0; i < xs.Length; i++)
+            {
+                mySL.Add(i, 0.21 * xs[i].R + 0.72 * xs[i].G + 0.07 * xs[i].B);
+            }
+            double[] arr = new double[xs.Length];
+            for (int i = 0; i < xs.Length; i++)
+            {
+                arr[i] = 0.21 * xs[i].R + 0.72 * xs[i].G + 0.07 * xs[i].B;
+            }
+            Array.Sort(arr);
+            double v = arr[arr.Length / 2];
+            int wynik = 0;
+            for (int i = 0; i < xs.Length; i++)
+            {
+                if (v == 0.21 * xs[i].R + 0.72 * xs[i].G + 0.07 * xs[i].B)
+                {
+                    wynik = i;
+                }
+            }
+            return wynik;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                modyfikuj(new double[,] { { 0, 0, 0 }, { 0, 1, -1 }, { 0, 0, 0 } });
+            }
+            else if (radioButton2.Checked)
+            {
+                modyfikuj(new double[,] { { 0, 0, 0 }, { 0, 1, 0 }, { 0, -1, 0 } });
+            }
+            else if (radioButton3.Checked)
+            {
+                modyfikuj(new double[,] { { 1, 1, 1 }, { 0, 0, 0 }, { -1, -1, -1 } });
+            }
+            else if (radioButton4.Checked)
+            {
+                modyfikuj(new double[,] { { 1, 0, -1 }, { 1, 0, -1 }, { 1, 0, -1 } });
+            }
+            else if (radioButton5.Checked)
+            {
+                modyfikuj(new double[,] { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } });
+            }
+            else if (radioButton6.Checked)
+            {
+                modyfikuj(new double[,] { { 1, 0, -1 }, { 2, 0, -2 }, { 1, 0, -1 } });
+            }
+            else if (radioButton7.Checked)
+            {
+                modyfikuj(new double[,] { { 0, -1, 0 }, { -1, 4, -1 }, { 0, -1, 0 } });
+            }
+            else if (radioButton8.Checked)
+            {
+                max();
+            }
+            else if (radioButton9.Checked)
+            {
+                min();
+            }
+            else if (radioButton10.Checked)
+            {
+                median();
+            }
+        }
+    }
+}
